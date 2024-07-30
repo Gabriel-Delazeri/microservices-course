39d69d4b99c500b21b11939bfd25dfdccc5a96c4
Gabriel-Delazeri <gbldelazeri@gmail.com>
2024-07-30 20:26:52 -0300

    feat: utilizing real data instead of mocks to get cambio

--- cambio-service/src/main/java/com/delazeri/cambioservice/controllers/CambioController.java
+++ cambio-service/src/main/java/com/delazeri/cambioservice/controllers/CambioController.java
@@ -1,6 +1,7 @@
 package com.delazeri.cambioservice.controllers;
 
 import com.delazeri.cambioservice.models.Cambio;
+import com.delazeri.cambioservice.repositories.CambioRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping(value = "cambio-service")
 public class CambioController {
 
     private final Environment environment;
+    private final CambioRepository cambioRepository;
 
-    public CambioController(Environment environment) {
+    public CambioController(Environment environment, CambioRepository cambioRepository) {
         this.environment = environment;
+        this.cambioRepository = cambioRepository;
     }
 
     @GetMapping(value = "/{amount}/{from}/{to}")
@@ -26,8 +30,16 @@ public class CambioController {
             @PathVariable String from,
             @PathVariable String to
     ) {
-        String port = environment.getProperty("local.server.port");
+        Cambio cambio = cambioRepository.findByFromAndTo(from, to);
+        if (cambio == null) throw new RuntimeException("Currency unsupported");
 
-        return ResponseEntity.ok(new Cambio(1L, from, to, BigDecimal.ONE, amount, port));
+        String port = environment.getProperty("local.server.port");
+        cambio.setEnvironment(port);
+
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+
+        cambio.setConvertedValue(conversionFactor.multiply(amount).setScale(2, RoundingMode.CEILING));
+
+        return ResponseEntity.ok(cambio);
     }
 }
