6de09b50ee75d6fdba66a5d39f38e2726e7e850c
Gabriel-Delazeri <gbldelazeri@gmail.com>
2024-08-01 19:04:21 -0300

    refactor: using eureka server to locate the cambio service without url

--- book-service/src/main/java/com/delazeri/bookservice/clients/CambioClient.java
+++ book-service/src/main/java/com/delazeri/bookservice/clients/CambioClient.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cambio-service", url = "localhost:8000")
+@FeignClient(name = "cambio-service")
 public interface CambioClient {
 
     @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
-    public CambioDto getCambio(
+    CambioDto getCambio(
             @PathVariable Double amount,
             @PathVariable String from,
             @PathVariable String to
